77d76a9427d03367c7a0f0954b63021f55b224d5
Arnaldur <aarnaldur@gmail.com>
2021-11-30 13:52:53 +0000

    added json file reading

--- src/models/models.py
+++ src/models/models.py
@@ -1,13 +1,20 @@
 from dataclasses import dataclass
+import json
+from typing import Dict, Any
+import dataclasses
 
 
 @dataclass
 class Id:
     value: int
 
+class Model:
+    @classmethod
+    def from_dict(cls, dictionary: Dict[str, Any]):
+        return cls(**dictionary)
 
 @dataclass
-class BaseEmployee:
+class BaseEmployee(Model):
     id: Id
     name: str
     mobile_number: str
--- src/storage/employee_storage.py
+++ src/storage/employee_storage.py
@@ -1,4 +1,5 @@
 import json
+import pathlib
 
 from ..models.models import Employee
 
@@ -9,14 +10,16 @@ FILE_PATH = "data/Employee.json"
 class EmployeeStorage:
     @staticmethod
     def get(id: int):
+        print("get")
         with open(FILE_PATH, encoding="utf-8") as file:
             data = json.load(file)
-            print("This is the employee storage")
             print(data[id])
 
     @staticmethod
     def get_all():
-        pass
+        with open(FILE_PATH, 'r', encoding="utf-8", ) as file:
+            data = json.load(file)
+            return [Employee.from_dict(emp) for emp in data]
 
     def add(entity: Employee):
         pass
