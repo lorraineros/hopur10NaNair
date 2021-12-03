ce0849535ce3766c556ee03ba8d9bd9e8b35cb60
Andriy Fedorets <andriy21@ru.is>
2021-12-03 12:25:50 +0000

    ok

--- src/ui/destination_ui.py
+++ src/ui/destination_ui.py
@@ -7,45 +7,32 @@ from src.models.models import Destination
 
 class DestinationMenu(AbstractMenu):
     def show(self):
-        '''This function shows the menu for Real Estate '''
-        print("--- Destination ---")
-        print("1. List of all destinations")
-        print("2. Add new Destination")
-        print("3. Delete Destination")
-        print("b. Back")
-        print("q. Quit")
+        print("""
+--- Destination Menu ---
+1. List of all destinations   
+2. Add new Destination
+3. Delete destination 
+
+q. Quit
+b. Back
+        """)
+
+    # def handle_input(self, command):
+    #     return super().handle_input(command)
 
     def handle_input(self, command):
-        '''This function handles input for the RealEstateMenu'''
-        if command == "1":
-            pass
-        elif command == "2":
-            pass
-        elif command == "3":
-            pass
-        elif command == "b":
-            return "back"
-        elif command == "q":
-            return "quit"
+            if command == '1':
+                self.list_destinations()
+            elif command == '2':
+                pass
+                # return CreationMenu(Destination)
+            elif command == '3':
+                self.delete_destination()
+            elif command == "b":
+                return "back"
+            elif command == "q":
+                return "quit"
 
-    # def user_input(self):
-    #     while True:
-    #         print()
-    #         command = input("> ")
-    #         print()
-    #         if command == '1':
-    #             self.list_destinations()
-    #         elif command == '2':
-    #             pass
-    #             # return CreationMenu(Destination)
-    #         elif command == '3':
-    #             self.delete_destination()
-    #         elif command == "b":
-    #             return "back"
-    #         elif command == "q":
-    #             return "quit"
-    #         else:
-    #             print("Invalid option, try again!")
 
     def list_destinations(self):
         print(f"{'--- List of Destinations ---':^34}")
@@ -59,10 +46,10 @@ class DestinationMenu(AbstractMenu):
 
     def delete_destination(self):
         print("""
-        --- Delete a Destination ---
-        1. By ID   
-        2. By Name  
+--- Delete a Destination ---
+1. By ID   
+2. By Name  
 
-        q. Quit
-        b. Back
+q. Quit
+b. Back
                 """)
