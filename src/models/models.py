98bf70a94d04073ca5a4ad9a9dc53a7ac22d1930
Arnaldur Bjarnason <a.arnaldur+git@gmail.com>
2021-12-04 19:41:05 +0000

    gave all models a default value

--- src/models/models.py
+++ src/models/models.py
@@ -8,12 +8,13 @@ M = TypeVar("M")
 
 @dataclass
 class Id:
-    value: int
+    idx: int = 0
+    model: str = ""
 
 
 @dataclass
 class Model:
-    id: Id = field(metadata={"autoinit": True})
+    id: Id = field(default=Id(), metadata={"autoinit": True})
 
     @classmethod
     def from_dict(cls, dictionary: Dict[str, Any]):
@@ -22,45 +23,45 @@ class Model:
 
 @dataclass
 class BaseEmployee(Model):
-    name: str
-    phone: str
+    name: str = field(default="")
+    phone: str = field(default="")
 
 
 @dataclass
 class Employee(BaseEmployee):
-    home_address: str
-    password: str
-    gsm: str
-    email: str
-    is_manager: bool
-    home_address: str
-    work_destination: Id
+    home_address: str = field(default="")
+    password: str = field(default="")
+    gsm: str = field(default="")
+    email: str = field(default="")
+    is_manager: bool = field(default=False)
+    home_address: str = field(default="")
+    work_destination: Id = field(default=Id())
 
 
 @dataclass
 class WorkRequest(Model):
-    title: str
-    location: str
-    real_estate: str
-    employee: int
-    contractor: int
-    start_date: str
-    end_date: str
-    description: str
-    priority: str
-    repeated_work: int
+    title: str = field(default="")
+    location: str = field(default="")
+    real_estate: str = field(default="")
+    employee: Id = field(default=Id())
+    contractor: Id = field(default=Id())
+    start_date: str = field(default="")
+    end_date: str = field(default="")
+    description: str = field(default="")
+    priority: str = field(default="")
+    repeated_work: int = field(default=0)
 
 
 @dataclass
 class RealEstate(Model):
-    address: str
-    real_estate_number: str
-    condition: str
-    facilities: str
-    type_of_real_estate: str
-    rooms: int
-    size: int
-    destination: Id
+    address: str = field(default="")
+    real_estate_number: str = field(default="")
+    condition: str = field(default="")
+    facilities: str = field(default="")
+    type_of_real_estate: str = field(default="")
+    rooms: int = field(default=0)
+    size: int = field(default=0)
+    destination: Id = field(default=Id())
 
     def __str__(self):
         return """
@@ -83,9 +84,9 @@ Size: {} """.format(
 
 @dataclass
 class Contractor(BaseEmployee):
-    name_of_contact: str
-    working_hours: str
-    location: int
+    name_of_contact: str = field(default="")
+    working_hours: str = field(default="")
+    location: int = field(default=0)
 
     def __str__(self):
         return """
@@ -106,9 +107,9 @@ Location: {}""".format(
 
 @dataclass
 class Destination(Model):
-    id: Id
-    name: str
-    country: str
+    id: Id = field(default=Id())
+    name: str = field(default="")
+    country: str = field(default="")
 
     def __str__(self):
         return """
