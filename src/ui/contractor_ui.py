b79f52a36f7ab8f8010636df7f60b0e7d2a2852e
Andri Þór Tryggvason <andrit20@ru.is>
2021-12-05 17:32:13 +0000

    Update contractor_ui.py

--- src/ui/contractor_ui.py
+++ src/ui/contractor_ui.py
@@ -1,3 +1,4 @@
+from src.models.models import Contractor
 from src.logic.logic_api import LogicAPI
 from src.ui.abstract_menu import AbstractMenu
 from ..logic.contractor_logic import ContractorLogic
@@ -35,24 +36,22 @@ class ContractorMenu(AbstractMenu):
             f"| {'ID':^3} | {'Name':^16} | {'Location':^16} |"
         )
         print("-" * 45)
-        for contr in LogicAPI.contractor_list():
+        for (contr_id, contr) in LogicAPI().get_all(Contractor).items():
             print(f"| {contr.id:<3} | {contr.name:<16} | {contr.location:<16} |")
         print("-" * 45)
         print()
 
     def find_contractor(self):
         id_input = input("Enter ID to choose a contractor: ")
-        is_id = LogicAPI.contractor_id_check(id_input)
+        is_id = LogicAPI().contractor_id_check(id_input)
 
         while not is_id:
             print("Sorry did not find address, try again.")
             id_input = input("Enter ID to choose a contractor: ")
-            is_id = LogicAPI.contractor_id_check(id_input)
+            is_id = LogicAPI().contractor_id_check(id_input)
 
-        for contr in LogicAPI.contractor_list():
-            if contr.id == int(id_input):
-                print(contr)
-
+        contr = LogicAPI().get(Contractor, int(id_input))
+        print(contr)
         print()
 
 
