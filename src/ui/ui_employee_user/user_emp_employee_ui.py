d02f51a4ccadb83ce700061fa2b4dc62ad436281
Andri Þór Tryggvason <andrit20@ru.is>
2021-12-06 11:56:43 +0000

    Update user_emp_employee_ui.py

--- src/ui/ui_employee_user/user_emp_employee_ui.py
+++ src/ui/ui_employee_user/user_emp_employee_ui.py
@@ -1,5 +1,6 @@
-from src.ui.employee_ui import EmployeeListMenu, EmployeeMenu, FindEmployee
-
+from src.ui.employee_ui import EmployeeMenu, FindEmployee
+from src.logic.logic_api import LogicAPI
+from src.ui.common_menus import BackQuitMenu
 
 class EmployeeMenuUserEmp(EmployeeMenu):
     def show(self):
@@ -10,9 +11,45 @@ class EmployeeMenuUserEmp(EmployeeMenu):
 
     def handle_input(self, command):
         if command == "1":
-            return FindEmployee()
+            return FindEmployeeUserEmp()
         elif command == "2":
-            return EmployeeListMenu()
+            yes_no_input = input("Do you want to display a list of employees by destination (Y/N)? ")
+            is_yes_no = LogicAPI().yes_no_check(yes_no_input)
+
+            while not is_yes_no:
+                print("Sorry, did not understand that, try again.")
+                yes_no_input = input("Do you want to display a list of employees by destination (Y/N)? ")
+                is_yes_no = LogicAPI().yes_no_check(yes_no_input)
+
+            if yes_no_input.upper() == "Y":
+                self.display_employees_by_dest()
+            elif yes_no_input.upper() == "N":
+                self.display_all_employees()
+            
+            return BackQuitMenu()
+        elif command == "b":
+            return "back"
+        elif command == "q":
+            return "quit"
+
+
+class FindEmployeeUserEmp(FindEmployee):
+    def show(self):
+        return super().show()
+
+    def handle_input(self, command):
+        if command == "1":
+            print("Find Employee by ID:")
+            print()
+            self.display_all_employees()
+            self.find_employee_by_id()
+            return BackQuitMenu()
+        elif command == "2":
+            print("Find Employee by Destination:")
+            print()
+            self.display_employees_by_dest()
+            self.find_employee_by_id()
+            return BackQuitMenu()
         elif command == "b":
             return "back"
         elif command == "q":
