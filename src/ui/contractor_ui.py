2e9f51ce113fcf19534a79fce1096c8fcb7b48ea
Andri Þór Tryggvason <andrit20@ru.is>
2021-12-10 15:07:09 +0000

    Added doc strings

--- src/ui/contractor_ui.py
+++ src/ui/contractor_ui.py
@@ -5,7 +5,7 @@ from src.ui.list_menu import EditPickerMenu
 
 
 class ContractorMenu(SimpleMenu):
-    """This class is for contract menu"""
+    '''The Menu for Contractor'''
 
     @property
     def header(self):
@@ -23,68 +23,3 @@ class ContractorMenu(SimpleMenu):
 
     def name(self):
         return f"Contractor Menu"
-
-    # def show(self):
-    #     print("1. Register a new contractor")
-    #     print("2. Display list of contractors")
-    #     print("b. Back")
-    #     print("q. Quit")
-
-    # def handle_input(self, command):
-    #     """This function handles input for contractor menu"""
-    #     if command == "1":
-    #         return CreationMenu(Contractor)
-    #     elif command == "2":
-    #         return EditPickerMenu(Contractor)
-    #     elif command == "b":
-    #         return "back"
-    #     elif command == "q":
-    #         return "quit"
-
-
-#     def display_all_contractors(self):
-#         """This function displays all contractors"""
-#         print(f"{'--- List of Contractor ---':^45}")
-#         print("-" * 45)
-#         print(f"| {'ID':^3} | {'Name':^16} | {'Location':^16} |")
-#         print("-" * 45)
-#         for (contr_id, contr) in LogicAPI().get_all(Contractor).items():
-#             contr_location = LogicAPI().get(Destination, contr.location)
-#             print(f"| {contr.id:<3} | {contr.name_of_company:<16} | {contr_location.name:<16} |")
-#         print("-" * 45)
-#         print()
-
-#     def find_contractor(self):
-#         """This function helps finding specific all contractors"""
-#         id_input = input("Enter ID to choose a contractor: ")
-#         is_id = LogicAPI().contractor_id_check(id_input)
-
-#         while not is_id:
-#             print("Sorry did not find address, try again.")
-#             id_input = input("Enter ID to choose a contractor: ")
-#             is_id = LogicAPI().contractor_id_check(id_input)
-
-#         contr = LogicAPI().get(Contractor, int(id_input))
-#         self.print_contractor(contr)
-#         print()
-
-#     def print_contractor(self, contr):
-#         """This function prints contractors"""
-#         contr_location = LogicAPI().get(Destination, contr.location)
-#         print(
-#             """
-# ID: {}
-# Name: {}
-# Name of Contact: {}
-# Phone Number: {}
-# Working hours: {}
-# Location: {}
-#         """.format(
-#                 contr.id,
-#                 contr.name,
-#                 contr.name_of_contact,
-#                 contr.phone,
-#                 contr.working_hours,
-#                 contr_location.name,
-#             )
-#         )
--- src/ui/destination_ui.py
+++ src/ui/destination_ui.py
@@ -5,7 +5,7 @@ from src.ui.list_menu import EditPickerMenu
 
 
 class DestinationMenu(SimpleMenu):
-    """This class controls destination menu"""
+    '''The Menu for Destination'''
 
     @property
     def header(self):
@@ -25,39 +25,3 @@ class DestinationMenu(SimpleMenu):
         return f"Destination Menu"
 
 
-#     def show(self):
-#         print(
-#             """
-# --- Destination Menu ---
-# 1. Register a new destination
-# 2. List of all destinations
-
-# q. Quit
-# b. Back
-#         """
-#         )
-
-#     def handle_input(self, command):
-#         """This function handles input for AbstractMenu"""
-
-#         if command == "1":
-#             return CreationMenu(Destination)
-#         elif command == "2":
-#             return EditPickerMenu(Destination)
-#         elif command == "b":
-#             return "back"
-#         elif command == "q":
-#             return "quit"
-
-# def list_of_all_destinations(self):
-#     """This function handles input for list of all destinations"""
-#     print(f"{'--- List of Destinations ---':^51}")
-#     print("-" * 51)
-#     print(f"| {'ID':^2} | {'Name':^21} | {'Country':^18} |")
-#     print("-" * 51)
-#     for (dest_id, destination) in LogicAPI().get_all(Destination).items():
-#         print(
-#             f"| {destination.id:<2} | {destination.name:<21} | {destination.country:<18} |"
-#         )
-#     print("-" * 51)
-#     print()
